7a12d3befc872bbe63be95b43c17d74a2df959ef
SHYPE <airscentes@gmail.com>
2026-05-31 03:30:54 +0200

    Add Solana wallet providers

---
+++ components/Providers.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { useMemo } from 'react';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const endpoint = 'https://api.mainnet-beta.solana.com';
+
+export default function Providers({ children }) {
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
