4e231baede414661dfe5a68e62059e80dbfc9ff8
SHYPE <airscentes@gmail.com>
2026-05-31 03:31:54 +0200

    Connect header wallet button

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 const logoUrl = 'https://raw.githubusercontent.com/airscentes-del/shype/main/IMG_2365.png';
 const items = [['/', 'Overview'], ['/swap', 'Swap'], ['/perps', 'Perps'], ['/stake', 'Stake'], ['/launch', 'Launch'], ['/vault', 'Vault']];
@@ -17,7 +18,7 @@ export default function Navigation() {
       <nav className="navRail">
         {items.map(([href, label]) => <Link key={href} className={pathname === href ? 'active' : ''} href={href}>{label}</Link>)}
       </nav>
-      <button className="walletButton" type="button">Connect wallet</button>
+      <WalletMultiButton className="walletButton" />
     </header>
   );
 }
